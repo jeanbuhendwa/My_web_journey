7663e49d39ae5fbe1cdfc742274a32c073e30a0c
Jean Jacques Buhendwa <jeanjacquesirenge40@gmail.com>
2022-03-04 11:00:48 +0300

    Starting my day3 with DOM manipulation

---
+++ Js/day4/script.js
@@ -0,0 +1,6 @@
+// document.getElementsByTagName("h1");
+// document.getElementById("first");
+// document.querySelector("Select the element");
+// document.querySelectorAll("select all the element here by tagname");
+// document.querySelector("select the tag here").getAttribute("get the attribute name here");
+// document.querySelector("select the tag element here").setAttribute("select the attribute", "Change it here"); 
