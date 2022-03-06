453a0a85290e585083c0dd629d8159f38c372bba
Jean Jacques Buhendwa <jeanjacquesirenge40@gmail.com>
2022-03-06 21:32:01 +0300

    Adding some JavaScript event listener

--- Js/day4/script.js
+++ Js/day4/script.js
@@ -4,3 +4,44 @@
 // document.querySelectorAll("select all the element here by tagname");
 // document.querySelector("select the tag here").getAttribute("get the attribute name here");
 // document.querySelector("select the tag element here").setAttribute("select the attribute", "Change it here"); 
+
+// var button = document.getElementsByTagName("button")[0];
+
+// button.addEventListener("click", function(){
+//     console.log("Click...");
+// });
+
+// button.addEventListener("mouseenter", function(){
+//     console.log("Mouse enter...");
+// })
+
+// button.addEventListener("mouseleave", function(){
+//     console.log("mouseleave...");
+// })
+
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength (){
+    return input.value.length;
+}
+function createElement (){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+}
+function addAfterClick (){
+    if (inputLength()>0){
+        createElement();
+    }
+}
+function addAfterKeyPress (event){
+    if (inputLength()>0 && event.keyCode === 13){
+        createElement();
+    }
+}
+button.addEventListener("click", addAfterClick);
+
+input.addEventListener("keypress", addAfterKeyPress);
