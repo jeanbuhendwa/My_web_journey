28436cea7031a111a77f8ba09bbb6f58de302ed7
Jean Jacques Buhendwa <jeanjacquesirenge40@gmail.com>
2022-03-07 19:25:05 +0300

    Adding a simple background generator

---
+++ Js/day5/script.js
@@ -0,0 +1,18 @@
+var body = document.getElementById("mainbody");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var css = document.querySelector("h3");
+
+// color1.addEventListener("input", function(){
+//     body.style.background = "linear-gradient(to right, "+ color1+ ","+ color2 +")";
+// })
+
+color1.addEventListener("input", function(){
+    body.style.background = "linear-gradient(to right, " +color1.value +"," +color2.value +")";
+    css.textContent = body.style.background + ";";
+})
+
+color2.addEventListener("input", function(){
+    body.style.background = "linear-gradient(to right, " +color1.value + "," + color2.value + ")";
+    css.textContent = body.style.background +";";
+})
